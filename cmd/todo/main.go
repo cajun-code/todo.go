f07668d2cc7e4d4d06b385e8695183729eb13788
Allan Davis <cajun.code@gmail.com>
2022-05-14 16:23:01 -0600

    added second test

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"shadeauxmedia.com/tools/todo"
 )
@@ -12,8 +15,26 @@ var todoFileName = ".todo.json"
 
 const envFileNameKey = "TODO_FILENAME"
 
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
+}
+
 func main() {
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	//task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to todo list")
 	list := flag.Bool("list", false, "List all tasks")
 	completed := flag.Int("complete", 0, "Item to be completed")
 
@@ -44,8 +65,14 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		// case *task != "":
+		task, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(task)
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -45,20 +46,32 @@ func TestTodoCLI(t *testing.T) {
 	}
 	cmdPath := filepath.Join(dir, binName)
 
-	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, "-task", task)
+	t.Run("AddNewTaskFromArgs", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add", task)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
-
+	task2 := "test task number 2"
+	t.Run("AndNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
 	t.Run("ListTasks", func(t *testing.T) {
 		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
 		}
-		expected := " 1: " + task + "\n"
+		expected := fmt.Sprintf(" 1: %s\n 2: %s\n", task, task2)
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q", expected, string(out))
 		}
